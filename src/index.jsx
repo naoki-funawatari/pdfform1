bafd211f459a7abf2524e95d30e1e13bf14b51b5
naoki-funawatari <40711643+naoki-funawatari@users.noreply.github.com>
2019-10-22 10:37:43 +0900

    ルーターを分離

--- src/index.jsx
+++ src/index.jsx
@@ -1,41 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { BrowserRouter, Route } from 'react-router-dom';
-// const Router = ReactRouter.Router;
-// const Route = ReactRouter.Route;
-// const IndexRoute = ReactRouter.IndexRoute;
-// const History = ReactRouter.History;
-
-import LoginForm from './loginform';
-import SelectForm from './selectform';
-import InputForm1 from './inputform1';
-import InputForm2 from './inputform2';
-import InputForm3 from './inputform3';
-
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    render() {
-        return (
-            <BrowserRouter>
-                <div>
-                    <Route exact path='/' component={LoginForm} />
-                    <Route path='/selectform' component={SelectForm} />
-                    <Route path='/inputform1' component={InputForm1} />
-                    <Route path='/inputform2' component={InputForm2} />
-                    <Route path='/inputform3' component={InputForm3} />
-                </div>
-            </BrowserRouter>
-        );
-    }
-}
+import Router from './Router';
 
 const app = document.getElementById('app');
 
 ReactDOM.render(
-    <App message="Hello React!" />,
+    <Router />,
     app
 );
---
+++ src/router.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import LoginForm from './loginform';
+import SelectForm from './selectform';
+import InputForm1 from './inputform1';
+import InputForm2 from './inputform2';
+import InputForm3 from './inputform3';
+
+export default class Router extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Route exact path='/' component={LoginForm} />
+                    <Route path='/selectform' component={SelectForm} />
+                    <Route path='/inputform1' component={InputForm1} />
+                    <Route path='/inputform2' component={InputForm2} />
+                    <Route path='/inputform3' component={InputForm3} />
+                </div>
+            </BrowserRouter>
+        );
+    }
+}
